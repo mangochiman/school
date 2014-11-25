2a381b0992452ce22f772a8bc5c4f93999486565
Ernest Matola <mangochiman@gmail.com>
2014-11-25 19:22:56 +0200

    Changing rails version to 2.3.5

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-School::Application.config.secret_token = '979eb11ef5e288c002bee73e5c57336e927f7a7278028c258079257b1ca559f98e14e41eaf65366b615161bb3c079526d5359336af6a0eaa2ff1cf8141b41567'
