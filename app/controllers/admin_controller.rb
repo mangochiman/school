6cdbe237b5f826707f0e66661a9b0b400fbeb4ca
Ernest Matola <mangochiman@gmail.com>
2014-11-27 14:07:40 +0200

    Working on layouts

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,6 +1,18 @@
 class AdminController < ApplicationController
   def home
-    
     render :layout => "application"
   end
+
+  def page_full_width
+     render :layout => false
+  end
+  
+  def page_other
+     render :layout => false
+  end
+
+  def dashboard
+   render :layout => false
+  end
+  
 end
