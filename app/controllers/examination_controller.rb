abd1a49c9f5b7647b39b6102141635e65eeb37c5
Ernest Matola <mangochiman@gmail.com>
2014-12-10 20:05:30 +0200

    Just started working on exam's

--- app/controllers/examination_controller.rb
+++ app/controllers/examination_controller.rb
@@ -4,23 +4,23 @@ class ExaminationController < ApplicationController
   end
 
   def new_exam_type
-    
+    render :layout => false
   end
 
   def edit_exam_type
-    
+    render :layout => false
   end
 
   def void_exam_type
-
+    render :layout => false
   end
   
   def assign_exam
-    
+    render :layout => false
   end
 
   def edit_exam_assignment
-    
+    render :layout => false
   end
   
 end
