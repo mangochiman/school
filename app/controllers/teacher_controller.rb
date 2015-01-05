fc1061a1747e446c6a46f332cfbf5dd9b8b9d3c6
Ernest Matola <mangochiman@gmail.com>
2015-01-05 19:47:29 +0200

    Working on Teacher's tab

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -25,37 +25,10 @@ class TeacherController < ApplicationController
   end
 
   def edit_teacher
-    if request.post?
-      teacher = Teacher.find(params[:teacher_id])
-      teacher.fname = params[:fname]
-      teacher.lname = params[:lname]
-      teacher.gender = params[:gender]
-      teacher.mobile = params[:mobile]
-      teacher.phone = params[:phone]
-      teacher.dob = params[:dob]
-      teacher.gender = params[:gender]
-      teacher.email = params[:email]
-      if teacher.save
-        flash[:notice] = "You have successfully edited the details"
-      else
-        flash[:error] = "Teacher #{fname} #{lname} could not be edited"
-      end
-      @teacher = teacher
-    else
-        @teacher = Teacher.find(params[:id])
-    end
-    @male = ""
-    @female = ""
-
-    if @teacher.gender.upcase == "MALE"
-      @male = "checked"
-    end
-
-    if @teacher.gender.upcase == "FEMALE"
-      @female = "checked"
-    end
-     render :layout => false
+    @teachers = Teacher.all
+    render :layout => false
   end
+
   def create
     if Teacher.create(:email => params[:email],
                             :password => params[:password],
@@ -72,4 +45,74 @@ class TeacherController < ApplicationController
     end
     redirect_to :controller => "teacher", :action => "add_teacher"
   end
+
+  def edit_me
+    teacher_id = params[:teacher_id]
+    @teacher = Teacher.find(teacher_id)
+    
+    if request.method == :post
+      if (@teacher.update_attributes({
+          :fname => params[:first_name],
+          :lname => params[:last_name],
+          :gender => params[:gender],
+          :email => params[:email],
+          :phone => params[:phone],
+          :dob => params[:dob].to_date
+        }))
+        flash[:notice] = "You have successfully edited the details"
+        redirect_to :controller => "teacher", :action => "edit_teacher" and return
+      else
+        flash[:error] = "Process aborted. Check for errors and try again"
+        redirect_to :controller => "teacher", :action => "edit_teacher" and return
+      end
+    end
+
+    render :layout => false
+  end
+
+  def search_teachers
+    first_name = params[:first_name]
+    last_name = params[:last_name]
+    gender = params[:gender]
+    conditions = ""
+    multiple = false
+    unless first_name.blank?
+      multiple = true
+      conditions += "fname LIKE '%#{first_name}%'"
+    end
+
+    unless last_name.blank?
+      multiple = true
+      conditions += ' AND ' unless conditions.blank?
+      conditions += "lname LIKE '%#{last_name}%' "
+    end
+
+    unless gender.blank?
+      conditions += ' AND ' if multiple
+      conditions += "gender = '#{gender}' "
+    end
+
+    unless conditions.blank?
+      teachers = Teacher.find_by_sql("SELECT * FROM teacher WHERE #{conditions}")
+    else
+      teachers = Teacher.all
+    end
+
+    hash = {}
+    
+    teachers.each do |teacher|
+      teacher_id = teacher.id.to_s
+      hash[teacher_id] = {}
+      hash[teacher_id]["fname"] = teacher.fname.to_s
+      hash[teacher_id]["lname"] = teacher.lname.to_s
+      hash[teacher_id]["phone"] = teacher.phone
+      hash[teacher_id]["email"] = teacher.email
+      hash[teacher_id]["gender"] = teacher.gender
+      hash[teacher_id]["dob"] = teacher.dob.to_date.strftime("%d-%b-%Y")
+      hash[teacher_id]["join_date"] = teacher.created_at.to_date.strftime("%d-%b-%Y")
+    end
+
+    render :json => hash
+  end
+  
 end
