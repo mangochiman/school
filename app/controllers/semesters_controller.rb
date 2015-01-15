2c7d1d8acf4bef44fdb7424b77d53fc4d61ea202
Ernest Matola <mangochiman@gmail.com>
2015-01-15 07:56:57 +0200

    Working on Semester's tab

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -12,6 +12,39 @@ class SemestersController < ApplicationController
   end
 
   def set_current_semester
+    if (request.method == :post)
+      current_semester = params[:semester]
+      current_semester_start_date = params[:start_date]
+      current_semester_end_date = params[:end_date]
+
+      ActiveRecord::Base.transaction do
+        cs = GlobalProperty.find_by_property("current_semester")
+        cs.delete unless cs.blank?
+
+        cssd = GlobalProperty.find_by_property("current_semester_start_date")
+        cssd.delete unless cssd.blank?
+
+        csed = GlobalProperty.find_by_property("current_semester_end_date")
+        csed.delete unless csed.blank?
+        
+        GlobalProperty.create(
+             {
+              :property => "current_semester",
+              :value => current_semester
+             },
+             {
+              :property => "current_semester_start_date",
+              :value => current_semester_start_date
+             },
+             {
+              :property => "current_semester_end_date",
+              :value => current_semester_end_date
+             }
+        )
+      end
+
+      redirect_to :action => "index" and return
+    end
     render :layout => false
   end
 
