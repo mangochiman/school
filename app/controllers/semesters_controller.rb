c9607354fa3748ac800a2e9aff282f3c27e83092
Ernest Matola <mangochiman@gmail.com>
2015-01-15 07:08:55 +0200

    Working on Semester's tab

--- app/controllers/semesters_controller.rb
+++ app/controllers/semesters_controller.rb
@@ -1,2 +1,22 @@
 class SemestersController < ApplicationController
+  def index
+    render :layout => false
+  end
+
+  def semester_settings
+    render :layout => false
+  end
+
+  def set_total_semesters
+     render :layout => false
+  end
+
+  def set_current_semester
+    render :layout => false
+  end
+
+  def view_semesters
+    render :layout => false
+  end
+  
 end
--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,2 +1,4 @@
 class Semester < ActiveRecord::Base
+  set_table_name :semesters
+	set_primary_key :semester_id
 end
