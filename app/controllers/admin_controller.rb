bf997828168ac8a52aa23c133cd113581905d0a5
Ernest Matola <mangochiman@gmail.com>
2015-01-23 04:48:31 +0200

    Working on Dashboard Tab

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,5 +1,27 @@
 class AdminController < ApplicationController
   def home
+    if (request.method == :post)
+      year = params[:year]
+      enrollment_data = {}
+      males = Student.find_by_sql("SELECT * FROM student WHERE gender = 'MALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{year}").count
+
+      females = Student.find_by_sql("SELECT * FROM student WHERE gender = 'FEMALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{year}").count
+      
+      enrollment_data["males"] = males
+      enrollment_data["females"] = females
+      enrollment_data["year"] = year
+      render :text => enrollment_data.to_json and return
+    end
+
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = (start_year..end_year).to_a.reverse
+    @males = Student.find_by_sql("SELECT * FROM student WHERE gender = 'MALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    @females = Student.find_by_sql("SELECT * FROM student WHERE gender = 'FEMALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
     render :layout => false
   end
 
