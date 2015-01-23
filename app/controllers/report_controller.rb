ae9e7a46ade06e810666c7eaf4d36f5cebaa68be
Ernest Matola <mangochiman@gmail.com>
2015-01-23 20:01:12 +0200

    Just started working on report tab

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -2,4 +2,44 @@ class ReportController < ApplicationController
   def index
     render :layout => false
   end
+
+  def students_per_semester_report
+    render :layout => false
+  end
+
+  def students_per_year_report
+    render :layout => false
+  end
+
+  def students_per_class_report
+    render :layout => false
+  end
+
+  def courses_report
+    render :layout => false
+  end
+  
+  def courses_per_class_report
+    render :layout => false
+  end
+
+  def teachers_report
+    render :layout => false
+  end
+
+  def teachers_per_subjects_report
+    render :layout => false
+  end
+
+  def results_per_class_report
+    render :layout => false
+  end
+
+  def subject_pass_rate_report
+    render :layout => false
+  end
+
+  def employees_report
+    render :layout => false
+  end
 end
