829d5110adb22e2e32dea846486f8722e69498b6
Ernest Matola <mangochiman@gmail.com>
2015-01-24 05:19:57 +0200

    Working on reports tab

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -4,22 +4,58 @@ class ReportController < ApplicationController
   end
 
   def students_per_semester_report
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = ["ALL"]
+    @years += (start_year..end_year).to_a.reverse
+    @semesters = ["ALL"]
+    @semesters += Semester.find(:all).collect{|s|[s.semester_number, s.semester_id]}
     render :layout => false
   end
 
   def students_per_year_report
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = ["ALL"]
+    @years += (start_year..end_year).to_a.reverse
+    @semesters = ["ALL"]
+    @semesters += Semester.find(:all).collect{|s|[s.semester_number, s.semester_id]}
     render :layout => false
   end
 
   def students_per_class_report
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = ["ALL"]
+    @years += (start_year..end_year).to_a.reverse
+    @semesters = ["ALL"]
+    @semesters += Semester.find(:all).collect{|s|[s.semester_number, s.semester_id]}
+    @class_rooms = ["All"]
+    @class_rooms += ClassRoom.all.collect{|cr|[cr.name, cr.class_room_id]}
     render :layout => false
   end
 
   def courses_report
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = ["ALL"]
+    @years += (start_year..end_year).to_a.reverse
+    @semesters = ["ALL"]
+    @semesters += Semester.find(:all).collect{|s|[s.semester_number, s.semester_id]}
+    @class_rooms = ["All"]
+    @class_rooms += ClassRoom.all.collect{|cr|[cr.name, cr.class_room_id]}
     render :layout => false
   end
   
   def courses_per_class_report
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = ["ALL"]
+    @years += (start_year..end_year).to_a.reverse
+    @semesters = ["ALL"]
+    @semesters += Semester.find(:all).collect{|s|[s.semester_number, s.semester_id]}
+    @class_rooms = ["All"]
+    @class_rooms += ClassRoom.all.collect{|cr|[cr.name, cr.class_room_id]}
     render :layout => false
   end
 
