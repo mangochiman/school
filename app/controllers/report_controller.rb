0d15268b477c350fc1eee11632ab2f161daee6c1
Ernest Matola <mangochiman@gmail.com>
2015-01-24 13:59:23 +0200

    Working on reports tab

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -64,10 +64,29 @@ class ReportController < ApplicationController
   end
 
   def teachers_per_subjects_report
+    @teachers = ["All"]
+    @teachers += Teacher.all.collect{|t|
+        name = t.fname.to_s.capitalize + ' ' + t.lname.to_s.capitalize + ' (' + t.gender.first.capitalize + ')'
+        [name, t.teacher_id]
+      }
+
+    @courses = ["All"]
+    @courses += Course.all.collect{|c|[c.name, c.course_id]}
+    
     render :layout => false
   end
 
   def results_per_class_report
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = ["ALL"]
+    @years += (start_year..end_year).to_a.reverse
+
+    @class_rooms = ["All"]
+    @class_rooms += ClassRoom.all.collect{|cr|[cr.name, cr.class_room_id]}
+    
+    @courses = ["All"]
+    @courses += Course.all.collect{|c|[c.name, c.course_id]}
     render :layout => false
   end
 
