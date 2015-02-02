cba527569b097f58b42fbeeeaf888793ae38aecb
Ernest Matola <mangochiman@gmail.com>
2015-02-02 21:53:11 +0200

    Adding links to drop down menus

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -596,5 +596,16 @@ class StudentController < ApplicationController
           DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
     render :layout => false
   end
+
+  def warning_letters_dashboard
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = (start_year..end_year).to_a.reverse
+    @males = Student.find_by_sql("SELECT * FROM student WHERE gender = 'MALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    @females = Student.find_by_sql("SELECT * FROM student WHERE gender = 'FEMALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    render :layout => false
+  end
   
 end
