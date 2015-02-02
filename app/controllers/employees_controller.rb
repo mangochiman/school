8d635a4c4bc1604a1161d06ca3af0619b1411d34
Ernest Matola <mangochiman@gmail.com>
2015-02-02 18:27:47 +0200

    Adding links to drop down menus

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,14 @@
 class EmployeesController < ApplicationController
+  def employee_registration_menu
+    render :layout => false
+  end
+  
+  def employee_management_menu
+    render :layout => false
+  end
+  
+  def position_management_menu
+    render :layout => false
+  end
+  
 end
