4253d2fa7c859307424b9be35b9d521e617e5100
Ernest Matola <mangochiman@gmail.com>
2015-02-02 21:00:41 +0200

    Adding links to drop down menus

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -281,4 +281,27 @@ class AdminController < ApplicationController
   def backup_data_menu
     render :layout => false
   end
+
+  def time_table_dashboard
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = (start_year..end_year).to_a.reverse
+    @males = Student.find_by_sql("SELECT * FROM student WHERE gender = 'MALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    @females = Student.find_by_sql("SELECT * FROM student WHERE gender = 'FEMALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    render :layout => false
+  end
+
+  def subject_groups_dashboard
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = (start_year..end_year).to_a.reverse
+    @males = Student.find_by_sql("SELECT * FROM student WHERE gender = 'MALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    @females = Student.find_by_sql("SELECT * FROM student WHERE gender = 'FEMALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    render :layout => false
+  end
+  
 end
--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -530,4 +530,16 @@ class StudentController < ApplicationController
   def warning_letters_menu
     render :layout => false
   end
+
+  def student_dashboard
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = (start_year..end_year).to_a.reverse
+    @males = Student.find_by_sql("SELECT * FROM student WHERE gender = 'MALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    @females = Student.find_by_sql("SELECT * FROM student WHERE gender = 'FEMALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    render :layout => false
+  end
+
 end
