29ae6174f9242544ba179f92d3edbfa62e024635
Ernest Matola <mangochiman@gmail.com>
2015-02-05 20:41:35 +0200

    Displaying available courses and also filtering by class

--- app/controllers/course_controller.rb
+++ app/controllers/course_controller.rb
@@ -35,14 +35,41 @@ class CourseController < ApplicationController
     
     course_ids = params[:course_ids].split(",")
     (course_ids || []).each do |course_id|
-     course = Course.find(course_id)
-     course.delete
+      course = Course.find(course_id)
+      course.delete
     end
     render :text => "true" and return
   end
   
   def view_courses
+    @class_rooms = [["All", "All"]]
+    @class_rooms += ClassRoom.find(:all).collect{|c|[c.name, c.id]}
     @courses = Course.all
+
+    if (request.method == :post)
+      hash = {}
+      unless (params[:class_room_id].match(/ALL/i))
+        class_room_id = params[:class_room_id]
+        class_room_courses = ClassRoomCourse.find(:all, :conditions => ["class_room_id IN (?)", class_room_id])
+        
+        (class_room_courses || []).each do |crc|
+          course_id = crc.course_id
+          hash[course_id] = {}
+          hash[course_id]["course_name"] = crc.course.name
+          hash[course_id]["date_created"] = crc.course.created_at.to_date.strftime("%d-%b-%Y")
+        end
+      else
+        courses = Course.all
+        (courses || []).each do |course|
+          course_id = course.course_id
+          hash[course_id] = {}
+          hash[course_id]["course_name"] = course.name
+          hash[course_id]["date_created"] = course.created_at.to_date.strftime("%d-%b-%Y")
+        end
+      end
+      render :text => hash.to_json and return
+    end
+    
     render :layout => false
   end
 
@@ -50,8 +77,8 @@ class CourseController < ApplicationController
     @course = Course.find(params[:course_id])
     if request.method == :post
       if (@course.update_attributes({
-          :name => params[:course_name]
-        }))
+              :name => params[:course_name]
+            }))
         flash[:notice] = "You have successfully edited the details"
         redirect_to :action => "edit_course" and return
       else
@@ -64,8 +91,8 @@ class CourseController < ApplicationController
   
   def create
     if (Course.create({
-        :name => params[:course_name]
-      }))
+            :name => params[:course_name]
+          }))
       flash[:notice] = "Operation successful"
       redirect_to :controller => "course", :action => "add_course"
     else
