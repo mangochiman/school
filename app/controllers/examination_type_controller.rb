f70121f5c0c7dd62c5c327d4e5f20e31e33eac36
Ernest Matola <mangochiman@gmail.com>
2015-02-09 21:04:09 +0200

    Added Exam Type Management drop down option under System Administration

--- app/controllers/examination_type_controller.rb
+++ app/controllers/examination_type_controller.rb
@@ -5,12 +5,12 @@ class ExaminationTypeController < ApplicationController
   
   def new_exam_type
     if (request.method == :post)
-        if (ExaminationType.create({
-          :name => params[:exam_type]
-        }))
-          flash[:notice] = "You have successfully added exam type"
-          redirect_to :action => "new_exam_type" and return
-        end
+      if (ExaminationType.create({
+              :name => params[:exam_type]
+            }))
+        flash[:notice] = "You have successfully added exam type"
+        redirect_to :action => "new_exam_type" and return
+      end
     end
 
     render :layout => false
@@ -25,16 +25,16 @@ class ExaminationTypeController < ApplicationController
     @exam_type = ExaminationType.find(params[:exam_type_id])
 
     if (request.method == :post)
-        if (@exam_type.update_attributes({
-          :name => params[:exam_type]
-        }))
+      if (@exam_type.update_attributes({
+              :name => params[:exam_type]
+            }))
 
-          flash[:notice] = "You have successfully edited exam type"
-          redirect_to :action => "edit_exam_type" and return
-        else
-          flash[:error] = "Operation aborted"
-          redirect_to :action => "edit_me", :exam_type_id => params[:exam_type_id]
-        end
+        flash[:notice] = "You have successfully edited exam type"
+        redirect_to :action => "edit_exam_type" and return
+      else
+        flash[:error] = "Operation aborted"
+        redirect_to :action => "edit_me", :exam_type_id => params[:exam_type_id]
+      end
     end
 
     render :layout => false
@@ -53,9 +53,9 @@ class ExaminationTypeController < ApplicationController
     end
 
     exam_type_ids = params[:exam_type_ids].split(",")
-      (exam_type_ids || []).each do |exam_type_id|
-       exam_type = ExaminationType.find(exam_type_id)
-       exam_type.delete
+    (exam_type_ids || []).each do |exam_type_id|
+      exam_type = ExaminationType.find(exam_type_id)
+      exam_type.delete
     end
 
     render :text => "true" and return
@@ -65,5 +65,15 @@ class ExaminationTypeController < ApplicationController
     @exam_types = ExaminationType.all
     render :layout => false
   end
-  
+
+  def manage_exam_type_dashboard
+    start_year = Date.today.year - 5
+    end_year = Date.today.year
+    @years = (start_year..end_year).to_a.reverse
+    @males = Student.find_by_sql("SELECT * FROM student WHERE gender = 'MALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    @females = Student.find_by_sql("SELECT * FROM student WHERE gender = 'FEMALE' AND
+          DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
+    render :layout => false
+  end
 end
