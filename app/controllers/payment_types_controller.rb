4ff5258ed2ad3862c85585e027cffbb42f213732
Ernest Matola <mangochiman@gmail.com>
2015-02-12 20:11:07 +0200

    Working on student payments

--- app/controllers/payment_types_controller.rb
+++ app/controllers/payment_types_controller.rb
@@ -1,2 +1,27 @@
 class PaymentTypesController < ApplicationController
+
+  def payment_types_menu
+    render :layout => false
+  end
+
+  def payment_types_management_dashboard
+    render :layout => false
+  end
+
+  def add_payment_type
+    render :layout => false
+  end
+
+  def edit_payment_type
+    render :layout => false
+  end
+
+  def void_payment_types
+    render :layout => false
+  end
+
+  def view_payment_types
+    render :layout => false
+  end
+  
 end
--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,27 @@
 class PaymentsController < ApplicationController
+
+  def payments_management_menu
+    render :layout => false
+  end
+
+  def payments_management_dashboard
+    render :layout => false
+  end
+
+  def add_payment
+    render :layout => false
+  end
+
+  def edit_payment
+    render :layout => false
+  end
+
+  def void_payments
+    render :layout => false
+  end
+
+  def view_payments
+    render :layout => false
+  end
+
 end
