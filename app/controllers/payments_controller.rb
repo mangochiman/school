854288f02e4495e2282566a87161c0ce8401ba14
Ernest Matola <mangochiman@gmail.com>
2015-02-13 20:41:30 +0200

    Working on student payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -9,9 +9,10 @@ class PaymentsController < ApplicationController
   end
 
   def add_payment
+    @students = Student.find(:all)
     render :layout => false
   end
-
+  
   def edit_payment
     render :layout => false
   end
@@ -24,4 +25,18 @@ class PaymentsController < ApplicationController
     render :layout => false
   end
 
+  def add_student_payment
+    @student = Student.find(params[:student_id])
+
+    @payment_types = [["[Payment Type]", ""]]
+    @payment_types += PaymentType.all.collect{|c|[c.name, c.id]}
+    
+    @semesters = [["[Semester]", ""]]
+    @semesters += Semester.all.collect{|s|[s.semester_number, s.id]}
+    
+    @current_student_payments = Payment.find(:all, :conditions => ["student_id =?",
+        params[:student_id]])
+    render :layout => false
+  end
+  
 end
