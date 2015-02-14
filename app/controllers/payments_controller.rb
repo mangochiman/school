ef9d27c92af219b0e9ce7639132cc10ba8336d38
Ernest Matola <mangochiman@gmail.com>
2015-02-14 11:09:18 +0200

    Working on students payments

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -39,6 +39,46 @@ class PaymentsController < ApplicationController
     
     render :layout => false
   end
+
+  def edit_particular_payment
+    payment_id = params[:payment_id]
+    @payment = Payment.find(payment_id)
+    @student = Student.find(@payment.student_id)
+    previous_payments = @student.payments
+    @payments_hash = {}
+    @payment_types_hash = {}
+    (PaymentType.all || []).each do |payment_type|
+      @payment_types_hash[payment_type.id] = payment_type.name
+    end
+    
+    previous_payments.each do |payment|
+      payment_type_id = payment.payment_type_id
+      @payments_hash[payment_type_id] = {} if @payments_hash[payment_type_id].blank?
+      latest_payment = @student.payments.find(:first, :order => "DATE(date) DESC",
+        :conditions => ["payment_type_id =?", payment_type_id]) if @payments_hash[payment_type_id].blank?
+
+      @payments_hash[payment_type_id]["amount_required"] = payment.payment_type.amount_required.to_i
+      @payments_hash[payment_type_id]["amount_paid"] = 0 if @payments_hash[payment_type_id]["amount_paid"].blank?
+      @payments_hash[payment_type_id]["amount_paid"] += payment.amount_paid.to_i
+      @payments_hash[payment_type_id]["balance"] = payment.payment_type.amount_required.to_i if @payments_hash[payment_type_id]["balance"].blank?
+      @payments_hash[payment_type_id]["balance"] -= payment.amount_paid.to_i
+      @payments_hash[payment_type_id]["latest_date_paid"] = latest_payment.date.to_date.strftime("%d-%b-%Y") if latest_payment
+    end
+    
+    if request.method == :post
+      amount_paid = params[:amount]
+      date_paid = params[:payment_date]
+      if (@payment.update_attributes({:amount_paid => amount_paid, :date => date_paid}))
+        flash[:notice] = "Operation successful"
+        redirect_to :controller => "payments", :action => "edit_my_payments_menu", :student_id => @student.student_id and return
+      else
+        flash[:error] = "Unable to save the details. Check for the errors and try again"
+        render :controller => "payments", :action => "edit_my_payments_menu", :student_id => @student.student_id and return
+      end
+    end
+    
+    render :layout => false
+  end
   
   def void_payments
     render :layout => false
