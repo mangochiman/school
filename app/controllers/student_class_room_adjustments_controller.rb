207990046e4ad1a1c975ba14a4b61723da8f02f8
Ernest Matola <mangochiman@gmail.com>
2015-02-15 08:22:56 +0200

    Created migrations for student-class_room-adjustments

---
+++ app/controllers/student_class_room_adjustments_controller.rb
@@ -0,0 +1,2 @@
+class StudentClassRoomAdjustmentsController < ApplicationController
+end
---
+++ test/unit/helpers/student_class_room_adjustments_helper_test.rb
@@ -0,0 +1,4 @@
+require 'test_helper'
+
+class StudentClassRoomAdjustmentsHelperTest < ActionView::TestCase
+end
