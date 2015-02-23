1c6ada730b76ae0ee9dc29be17fab8da12929583
Ernest Matola <mangochiman@gmail.com>
2015-02-23 20:56:13 +0200

    Working on User Management

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -34,5 +34,64 @@ class UserController < ApplicationController
           DATE_FORMAT(created_at, '%Y') = #{Date.today.year}").count
     render :layout => false
   end
+
+  def new_user
+    @users = User.find(:all)
+    render :layout => false
+  end
+
+  def edit_user
+    if (params[:user_id])
+      @user = User.find(params[:user_id])
+    end
+    @users = User.find(:all)
+    render :layout => false
+  end
   
+  def create
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    errors = ""
+    user_exists = User.find_by_username(params[:username])
+    errors += 'Username already exists.' if user_exists
+    errors += ' Password mismatch.' if (password != password_confirm)
+    
+    unless (errors.blank?)
+      flash[:error] = "#{errors}"
+      redirect_to :controller => "user", :action => "new_user" and return
+    end
+    
+    user = User.create({
+        :password => password,
+        :username => params[:username]
+      })
+    if user
+      flash[:notice] = "Operation successful"
+      redirect_to :controller => "user", :action => "new_user" and return
+    end
+  end
+
+  def update_user_data
+    if params[:user_id].blank?
+      flash[:error] = "You didn't select any user to edit. Select user first"
+      redirect_to :controller => "user", :action => "edit_user" and return
+    end
+    user = User.find(params[:user_id])
+    password = params[:password]
+    password_confirm = params[:password_confirm]
+    if (password != password_confirm)
+      flash[:error] = "Password mismatch."
+      redirect_to :controller => "user", :action => "edit_user", :user_id => params[:user_id] and return
+    end
+    if (user.update_attributes({
+            :username => params[:username],
+            :password => params[:password]
+          }))
+      flash[:notice] = "Operation successful"
+      redirect_to :controller => "user", :action => "edit_user" and return
+    else
+      flash[:error] = "Oops. Something wen't wrong. That's what we know"
+      redirect_to :controller => "user", :action => "edit_user" and return
+    end
+  end
 end
