8f8fef8ccf3cba5bda23ce456921a61189b31bf7
Ernest Matola <mangochiman@gmail.com>
2015-02-25 21:27:40 +0200

    Working on faculties

--- app/controllers/faculties_controller.rb
+++ app/controllers/faculties_controller.rb
@@ -3,4 +3,61 @@ class FacultiesController < ApplicationController
   def faculties_menu
     
   end
+
+  def add_faculty
+    @faculties = Faculty.find(:all)
+  end
+
+  def create_faculty
+    if (Faculty.create({
+            :name => params[:faculty_name]
+          }))
+      flash[:notice] = "Operation successful"
+      redirect_to :controller => "faculties", :action => "add_faculty" and return
+    else
+      flash[:error]  = "Unable to save. Check for errors and try again"
+      redirect_to :controller => "faculties", :action => "add_faculty" and return
+    end
+  end
+
+  def edit_faculty
+    if params[:faculty_id]
+      @faculty = Faculty.find(params[:faculty_id])
+    end
+    @faculties = Faculty.find(:all)
+    if request.method == :post
+      if (@faculty.update_attributes({
+              :name => params[:faculty_name]
+            }))
+        flash[:notice] = "Operation successful"
+        redirect_to :controller => "faculties", :action => "add_faculty" and return
+      else
+        flash[:error]  = "Unable to save. Check for errors and try again"
+        redirect_to :controller => "faculties", :action => "add_faculty" and return
+      end
+    end
+  end
+
+  def void_faculty
+    @faculties = Faculty.find(:all)
+  end
+
+  def view_faculties
+    @faculties = Faculty.find(:all)
+  end
+
+  def delete_faculties
+    if (params[:mode] == 'single_entry')
+      faculty = Faculty.find(params[:faculty_id])
+      faculty.delete
+      render :text => "true" and return
+    end
+
+    faculty_ids = params[:faculty_ids].split(",")
+    (faculty_ids || []).each do |faculty_id|
+      faculty = Faculty.find(faculty_id)
+      faculty.delete
+    end
+    render :text => "true" and return
+  end
 end
