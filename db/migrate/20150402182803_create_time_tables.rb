2442727e57381721b59f2188cd38acaaa2cd7a21
Ernest Matola <mangochiman@gmail.com>
2015-04-02 20:33:17 +0200

    Added timetable model

---
+++ db/migrate/20150402182803_create_time_tables.rb
@@ -0,0 +1,16 @@
+class CreateTimeTables < ActiveRecord::Migration
+  def self.up
+    create_table :time_table do |t|
+      t.integer :teacher_id
+      t.integer :class_room_id
+      t.integer :course_id
+      t.string :date
+      t.string :time
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :time_table
+  end
+end
