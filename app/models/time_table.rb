9452a3299293b66cfae87f115adf67338114bce1
Ernest Matola <mangochiman@gmail.com>
2015-04-02 20:48:03 +0200

    Added time_tables controller

--- app/models/time_table.rb
+++ app/models/time_table.rb
@@ -1,2 +1,10 @@
+require "composite_primary_keys"
+
 class TimeTable < ActiveRecord::Base
+  set_table_name :time_table
+  set_primary_keys :teacher_id, :class_room_id, :course_id, :date, :time
+
+  belongs_to :teacher
+  belongs_to :class_room
+  belongs_to :course
 end
--- db/migrate/20150402182803_create_time_tables.rb
+++ db/migrate/20150402182803_create_time_tables.rb
@@ -1,6 +1,6 @@
 class CreateTimeTables < ActiveRecord::Migration
   def self.up
-    create_table :time_table do |t|
+    create_table :time_table, :id => false do |t|
       t.integer :teacher_id
       t.integer :class_room_id
       t.integer :course_id
