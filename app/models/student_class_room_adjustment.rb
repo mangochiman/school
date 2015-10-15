ad36278744388b45e27d838dd79f34bc4b8ad9d1
mangochiman <mangochiman@gmail.com>
2015-10-15 06:16:47 +0200

    Adding start and end_dates before save

--- app/models/student_class_room_adjustment.rb
+++ app/models/student_class_room_adjustment.rb
@@ -6,5 +6,16 @@ class StudentClassRoomAdjustment < ActiveRecord::Base
   belongs_to :teacher
   belongs_to :semester
   belongs_to :class_room, :foreign_key => :new_class_room_id
-  
+
+  before_save :add_dates
+
+  def add_dates
+    start_date = GlobalProperty.find_by_property("current_semester_start_date").value
+    end_date = GlobalProperty.find_by_property("current_semester_end_date").value
+    semester = GlobalProperty.find_by_property("current_semester").value
+    
+    self.semester_id = semester
+    self.start_date = start_date
+    self.end_date = end_date
+  end
 end
