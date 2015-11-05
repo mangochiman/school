c33bc1db6e0988f82793f45be8cb56baa34c9317
mangochiman <mangochiman@gmail.com>
2015-11-05 20:55:02 +0200

    Working on archiving students

--- app/controllers/student_archives_controller.rb
+++ app/controllers/student_archives_controller.rb
@@ -21,5 +21,21 @@ class StudentArchivesController < ApplicationController
 
     render :text => "true" and return
   end
+
+  def activate_archived_students
+    if (params[:mode] == 'single_entry')
+      student = Student.find(params[:student_id])
+      student.student_archive.delete
+      render :text => "true" and return
+    end
+
+    student_ids = params[:student_ids].split(",")
+    (student_ids || []).each do |student_id|
+      student = Student.find(student_id)
+      student.student_archive.delete
+    end
+
+    render :text => "true" and return
+  end
   
 end
