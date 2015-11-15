39df16a92153943a5a986ce2134baa326987488d
mangochiman <mangochiman@gmail.com>
2015-11-15 08:04:18 +0200

    Searching payment types by name

--- app/controllers/payment_types_controller.rb
+++ app/controllers/payment_types_controller.rb
@@ -80,17 +80,36 @@ class PaymentTypesController < ApplicationController
 
   def search_payment_types
     payment_type_name = params[:payment_type_name]
+    order_by = params[:order_by]
+    order_by = 'amount_required ASC' if order_by.blank?
+
+    if (order_by.match(/DESC/i))
+      sort_direction = 'reverse'
+    else
+      sort_direction = 'forward'
+    end
+
     hash = {}
-    payment_types = PaymentType.find_by_sql("SELECT * FROM payment_type WHERE name LIKE '%#{payment_type_name}%'")
+    payment_types = PaymentType.find_by_sql("SELECT * FROM payment_type WHERE name LIKE '%#{payment_type_name}%' ORDER BY #{order_by}")
 
     payment_types.each do |payment_type|
-      payment_type_id = payment_type.payment_type_id
+      payment_type_id = payment_type.payment_type_id.to_s
       hash[payment_type_id] = {}
       hash[payment_type_id]["payment_type_name"] = payment_type.name
       hash[payment_type_id]["amount_set"] = ActionController::Base.helpers.number_to_currency(payment_type.amount_required, :unit => 'MK')
+      hash[payment_type_id]["amount_not_formatted"] = payment_type.amount_required
       hash[payment_type_id]["date_created"] = payment_type.created_at.to_date.strftime("%d-%b-%Y")
     end
+    raise Hash[hash.sort_by{|k, v|-v["amount_not_formatted"].to_i}].to_yaml
+    if (sort_direction == 'reverse')
+      #Hash[hash.sort_by{|k, v|v["amount_not_formatted"].to_i}.reverse] #Return back the hash after sorting. Hash sorting returns an array
+      x = Hash[hash.sort_by{|k, v|-v["amount_not_formatted"].to_i}]
+      raise x.to_yaml
+    else
+      hash = Hash[hash.sort_by{|k, v|-v["amount_not_formatted"].to_i}.reverse]
+    end #This is because the hash by default is sorting by key in regardless of the order by query. Just a hack to do the order by manually
 
+    raise hash.inspect
     render :json => hash and return
   end
 
